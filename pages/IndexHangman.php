bab4a7d4d38addaeac4b8cfd06b4cc6e4dceda88
Kiara Heirman <kiara.heirman@student.kdg.be>
2020-02-16 16:46:24 +0100

    Add changes to IndexHangman, Pictures and Hints

--- pages/IndexHangman.php
+++ pages/IndexHangman.php
@@ -1,6 +1,6 @@
 <?php 
 	session_start(); 
-	include ("../functionality/hangman.php");
+	include '../functionality/hangman.php';
 ?>
 
 <!DOCTYPE html>
@@ -8,28 +8,85 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Hangman</title>
-	<link rel="stylesheet" type="text/css" href="css/game.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-	<?php if (isset($_SESSION['start'])): ?> 
-		<h1>Game started</h1>
-		<form action="IndexHangman.php" method='POST'>
-			<h2>The word is: <?php foreach ($_SESSION['currentSolution'] as $key => $letter) {echo $key=$letter;}; ?></h2>
-			<?php foreach ($_SESSION['alphabet'] as $key => $value) {
-				echo "<input type='submit' name='letter-btn' value=" . $key=$value . ">";
+	<div id="game-box">
+		<?php if (isset($_SESSION['start'])): ?> 
+
+			<img src='images/LOGO.png' id='top-hangman-img' alt=''>
+			<h1>Game started</h1>
+
+			<?php switch ($_SESSION['currentAttemps']) {
+				case 1:
+					echo "<img src='images/Fase1.png' class='hangman-img' alt=''>";
+					break;
+				case 2:
+					echo "<img src='images/Fase2.png' class='hangman-img' alt=''>";
+					break;
+				case 3:
+					echo "<img src='images/Fase3.png' class='hangman-img' alt=''>";
+					break;
+				case 4:
+					echo "<img src='images/Fase4.png' class='hangman-img' alt=''>";
+					break;
+				case 5:
+					echo "<img src='images/Fase5.png' class='hangman-img' alt=''>";
+					break;
+				case 6:
+					echo "<img src='images/Fase6.png' class='hangman-img' alt=''>";
+					break;
+				case 7:
+					echo "<img src='images/Fase7.png' class='hangman-img' alt=''>";
+					break;
+				case 8:
+					echo "<img src='images/Fase8.png' class='hangman-img' alt=''>";
+					break;
+				case 9:
+					echo "<img src='images/Fase9.png' class='hangman-img' alt=''>";
+					break;
+				case 10:
+					echo "<img src='images/FullHangman.png' class='hangman-img' alt=''>";
+					break;
+				
+				default:
+					echo "<img src='images/Fase0.png' class='hangman-img' alt=''>";
+					break;
 			} ?>
+	
+			<form action="IndexHangman.php" method='POST'>
+				<div id="letters">
+					<?php foreach ($_SESSION['alphabet'] as $key => $value) {
+						echo "<input type='submit' class='letter-btn' name='letter-btn' value=" . $key=$value . ">";
+					} ?>
+				</div>
+				
+				<h2><?php foreach ($_SESSION['currentSolution'] as $key => $letter) {echo $key=$letter;}; ?></h2>
 
-			<h2>Current Attempts: <?php echo $_SESSION['currentAttemps']; ?></h2>
+				<h3>Current Attempts: <?php echo $_SESSION['currentAttemps']; ?></h3>
+				
+				<input type="submit" class="start-btn" name='hint-btn' value='Ask hint?'>
+				<input type="submit" class="start-btn" name='unset-btn' value='Quit game'>
 
-			<input type="submit" name='unset-btn' value='Quit game'>
-		</form>
-	<?php else: ?>
-		<h1>Game ended</h1>
-		<form action='IndexHangman.php' method='POST'>
-			<input type='submit' name='start-btn' value='Start Game'>
-		</form>
+				<?php if (isset($_POST['hint-btn'])) {
+					echo $_SESSION['hint'];
+				} ?>
+			</form>
 
-	<?php endif; ?> 
+		<?php else: ?>
+
+			<h1>Hangman</h1>
+			<img src="images/LOGO.png" alt="">
+			<?php if (isset($_SESSION['message'])) {
+				echo "<h3>". $_SESSION['message'] . "</h3>";
+			} ?>
+			<form action='IndexHangman.php' method='POST'>
+				<input type='submit' class="start-btn" name='start-btn' value='Start Game'>
+			</form>
+
+		<?php endif; ?> 			
+	</div>
+	
 		
 		
 		
